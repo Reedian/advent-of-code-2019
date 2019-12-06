aedd3f4d63c756461d393e7679cf92666043501a
Reedian <grzegorz.marek.jesionowski@gmail.com>
2019-12-06 16:13:58 +0100

    Solution for day6 second part

--- 06/orbits_count.rb
+++ 06/orbits_count.rb
@@ -13,6 +13,11 @@ class Orbit
     return 0 unless direct_orbit
     1 + direct_orbit.connections_count
   end
+
+  def children
+    return [] unless direct_orbit
+    [direct_orbit] + direct_orbit.children
+  end
 end
 
 
@@ -27,9 +32,15 @@ class OrbitList
     list.detect { |orbit| orbit.name == name } || initialize_orbit(name)
   end
 
+  def find_by_name(name)
+    list.detect { |orbit| orbit.name == name }
+  end
+
   def_delegators :list, :sum
   def_delegators :list, :size
   def_delegators :list, :map
+  def_delegators :list, :last
+  #def_delegators :list, :index
 
   private
 
@@ -42,7 +53,7 @@ class OrbitList
   end
 end
 
-def calc_orbits(input_file)
+def populate_orbit_list(input_file)
   orbit_list = OrbitList.new
   File.readlines(input_file).each do |line|
     a,b = line.strip.split(')')
@@ -50,5 +61,25 @@ def calc_orbits(input_file)
     orbit_b = orbit_list.find_or_initialize_by_name(b)
     orbit_b.direct_orbit = orbit_a
   end
+  orbit_list
+end
+
+def calc_orbits(input_file)
+  orbit_list = populate_orbit_list(input_file)
+
   orbit_list.sum(&:connections_count)
+end
+
+def calc_orbital_transfers(input_file)
+  orbit_list = populate_orbit_list(input_file)
+
+  you = orbit_list.find_by_name('YOU')
+  san = orbit_list.find_by_name('SAN')
+
+  intersection_point = (you.children & san.children).first
+
+  inter_you_index = you.children.index(intersection_point)
+  inter_san_index = san.children.index(intersection_point)
+
+  inter_san_index + inter_you_index
 end
--- 06/test_orbits_count.rb
+++ 06/test_orbits_count.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require_relative 'orbits_count'
 
 class TestOrbitsCount < Minitest::Test
-  def test_total_direct_and_indirect_orbits
+  def test_calc_orbits
     assert_equal(
       42,
       calc_orbits('test_input.txt')
@@ -11,6 +11,18 @@ class TestOrbitsCount < Minitest::Test
     assert_equal(
       314247,
       calc_orbits('input.txt')
+    )
+  end
+
+  def test_calc_orbital_transfers
+    assert_equal(
+      4,
+      calc_orbital_transfers('test_input_part_2.txt')
+    )
+
+    assert_equal(
+      514,
+      calc_orbital_transfers('input.txt')
     )
   end
 end
